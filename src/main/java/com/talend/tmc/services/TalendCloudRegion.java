b66f72d00b2d810fee0cd809b5f912f41e488139
batje <reinier@mountbatten.nl>
2021-09-09 14:20:04 +0200

    Updated URLs to API version 2.5

--- src/main/java/com/talend/tmc/services/TalendCloudRegion.java
+++ src/main/java/com/talend/tmc/services/TalendCloudRegion.java
@@ -4,22 +4,22 @@ public enum TalendCloudRegion {
 
     AWS_USA_EAST {
         public String toString() {
-            return "https://api.us.cloud.talend.com/tmc/v2.2/";
+            return "https://api.us.cloud.talend.com/tmc/v2.5/";
         }
     },
     AWS_EMEA {
         public String toString() {
-            return "https://api.eu.cloud.talend.com/tmc/v2.2/";
+            return "https://api.eu.cloud.talend.com/tmc/v2.5/";
         }
     },
     AWS_APAC {
         public String toString() {
-            return "https://api.ap.cloud.talend.com/tmc/v2.2/";
+            return "https://api.ap.cloud.talend.com/tmc/v2.5/";
         }
     },
     AZURE_USA_WEST {
         public String toString() {
-            return "https://api.us-west.cloud.talend.com/tmc/v2.2/";
+            return "https://api.us-west.cloud.talend.com/tmc/v2.5/";
         }
     }
 }
