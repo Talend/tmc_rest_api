815b8ed514e347a532028fab2f0516f0bbd8004e
batje <reinier@mountbatten.nl>
2021-11-10 10:05:45 +0100

    Updated ExecutableTaskService.java

--- src/main/java/com/talend/tmc/services/executables/ExecutableTaskService.java
+++ src/main/java/com/talend/tmc/services/executables/ExecutableTaskService.java
@@ -3,7 +3,7 @@ package com.talend.tmc.services.executables;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.talend.tmc.dom.Executable;
-import com.talend.tmc.dom.TaskNew;
+import com.talend.tmc.dom.Items;
 import com.talend.tmc.services.*;
 import org.springframework.http.HttpMethod;
 
@@ -34,61 +34,66 @@ public class ExecutableTaskService {
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
-    public TaskNew getById(String id) throws TalendRestException,
-             IOException, NullPointerException
+    public Executable[] getById(String id) throws TalendRestException,
+            IOException, NullPointerException
     {
         if (id == null) throw new NullPointerException("Value of id cannot be null");
-
-        TaskNew planResponse = null;
-        StringBuilder uri = new StringBuilder();
-        uri.append(region.toString()+path);
-        uri.append("/"+id);
-
-        Hashtable<Integer, String> response = client.call(HttpMethod.GET, uri.toString(), null);
-
-        for (Integer httpStatus : response.keySet())
-        {
-            String payload = response.get(httpStatus);
-            if (httpStatus != 200)
-            {
-                TalendError error = mapper.readValue(payload, TalendError.class);
-                throw new TalendRestException(error.toString());
-            } else {
-                planResponse = mapper.readValue(payload, TaskNew.class);
-            }
-
-        }
-
-        return planResponse;
+        return get( null, id);
     }
 
-    public Executable[] getByQuery(String fiqlQuery) throws TalendRestException,
+    public Executable[] getByQuery(String query) throws TalendRestException,
             IOException
     {
+        return get(query, null);
+    }
 
-        Executable[] executables = null;
+    public Executable[] getByName(String name, String workspace) throws TalendRestException,
+     IOException
+    {
+    	String query = "name=" + name;
+    	
+    	query = workspace != null ? query + "&workspaceId=" + workspace : query;
+    	
+    	return get(query, null);
+    }
+
+    private Executable[] get(String query, String id) throws TalendRestException,
+             IOException
+    {
+        Items items = null;
+        Executable executable = null;
+        Executable[] returnValue = null; 
+        
         StringBuilder uri = new StringBuilder();
         uri.append(region.toString()+path);
-        if (fiqlQuery != null) {
-            uri.append("?_s="+fiqlQuery);
+        if (query != null) {
+            uri.append("?" + query);
         }
+        if (id != null)
+            uri.append("/" + id);
 
         Hashtable<Integer, String> response = client.call(HttpMethod.GET, uri.toString(), null);
-
+ 
         for (Integer httpStatus : response.keySet())
         {
             String payload = response.get(httpStatus);
             if (httpStatus != 200)
             {
-                TalendError error = mapper.readValue(payload, TalendError.class);
+            	TalendError error = mapper.readValue(payload, TalendError.class);
                 throw new TalendRestException(error.toString());
             } else {
-                executables = mapper.readValue(payload, Executable[].class);
+            	if (id == null) {
+            		items = mapper.readValue(payload, Items.class);
+            		returnValue = items.getExecutables();
+            	} else {
+            		executable = mapper.readValue(payload, Executable.class);
+            		returnValue= new Executable[1];
+            		returnValue[0] = executable;
+            	}
             }
 
         }
 
-        return executables;
+        return returnValue;
     }
-
 }
